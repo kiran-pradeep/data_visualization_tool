8585adf0331ce02024e2451a2ab6c3621c8fe004
Kiran Pradeep <kira@Kirans-MacBook-Air.local>
2023-11-01 14:27:55 +0530

    modified the functions

--- backend/graph_functions.py
+++ backend/graph_functions.py
@@ -1,143 +1,146 @@
 import matplotlib.pyplot as plt
 
-
-def bar_plot(xs, ys, labels, colors, widths, xlabel, ylabel, title, out_path):
+# Function to create a line plot
+def create_line_plot(x_values, y_values, x_label, y_label, title, label):
     """
-    To create bar plots
+    Create and display a line plot.
 
     Args:
-        xs: list of lists for the x-axis
-        ys: list of lists for the y-axis
-        labels: list of labels for each corresponding (x, y) pairs
-        colors: list of colors for each corresponding (x, y) pairs
-        widths: list of widths for each corresponding (x, y) pairs
-        xlabel: label for the x-axis
-        ylabel: label for the y-axis
-        title: title for the graph
-        out_path: path to save the  graph
-
-    Returns:
-        None
+    x_values (list): X-axis values.
+    y_values (list): Y-axis values.
+    x_label (str): Label for the X-axis.
+    y_label (str): Label for the Y-axis.
+    title (str): Title of the plot.
+    label (str): Label for the line in the legend.
     """
-    plt.figure(figsize=(10,6))
-    for i, _ in enumerate(xs):
-        plt.bar(xs[i], ys[i], label=labels[i], color=colors[i], width=widths[i])
-    plt.legend()
-    plt.xlabel(xlabel)
-    plt.ylabel(ylabel)
-    plt.title(title)
+    fig, ax = plt.subplots()
+    ax.plot(x_values, y_values, label=label)
+    ax.set_xlabel(x_label)
+    ax.set_ylabel(y_label)
+    ax.set_title(title)
+    ax.legend()
 
-    plt.savefig(out_path, bbox_inches='tight')
-
-
-
-def scatter_plot(xs, ys, labels, colors, title, xlabel, ylabel, out_path):
+# Function to create a bar plot
+def create_bar_plot(categories, values, x_label, y_label, title, label):
     """
-    To create scatter plots 
+    Create and display a bar plot.
 
     Args:
-        xs: list of lists for the x-axis
-        ys: list of lists for the y-axis
-        labels: list of labels for each corresponding (x, y) pairs
-        colors: list of colors for each corresponding (x, y) pairs
-        xlabel: label for the x-axis
-        ylabel: label for the y-axis
-        title: title for the graph
-        out_path: path to save the  graph
-
-    Returns:
-        None
+    categories (list): Category names for the bars.
+    values (list): Values corresponding to each category.
+    x_label (str): Label for the X-axis.
+    y_label (str): Label for the Y-axis.
+    title (str): Title of the plot.
+    label (str): Label for the bars in the legend.
     """
-    plt.figure(figsize=(10,6))
-    for i, x in enumerate(xs):
-        plt.scatter(xs[i], ys[i], label=labels[i], color=colors[i])
-    plt.title(title)
-    plt.xlabel(xlabel)
-    plt.ylabel(ylabel)
-    plt.legend()
+    fig, ax = plt.subplots()
+    ax.bar(categories, values, label=label)
+    ax.set_xlabel(x_label)
+    ax.set_ylabel(y_label)
+    ax.set_title(title)
+    ax.legend()
 
-    plt.savefig(out_path, bbox_inches='tight')
-
-
-def pie_plot():
+# Function to create a scatter plot
+def create_scatter_plot(x_values, y_values, x_label, y_label, title, label):
     """
-    To generate pie plots 
+    Create and display a scatter plot.
 
     Args:
-        xs: list of lists for the x-axis
-        ys: list of lists for the y-axis
-        labels: list of labels for each corresponding (x, y) pairs
-        colors: list of colors for each corresponding (x, y) pairs
-        xlabel: label for the x-axis
-        ylabel: label for the y-axis
-        title: title for the graph
-        out_path: path to save the  graph
-
-    Returns:
-        None
+    x_values (list): X-axis values.
+    y_values (list): Y-axis values.
+    x_label (str): Label for the X-axis.
+    y_label (str): Label for the Y-axis.
+    title (str): Title of the plot.
+    label (str): Label for the points in the legend.
     """
-    plt.figure(figsize=(10,6))
+    fig, ax = plt.subplots()
+    ax.scatter(x_values, y_values, label=label)
+    ax.set_xlabel(x_label)
+    ax.set_ylabel(y_label)
+    ax.set_title(title)
+    ax.legend()
 
-    slice = [12, 25, 50, 36, 19]
-    activities = ['NLP','Neural Network', 'Data analytics', 'Quantum Computing', 'Machine Learning']
-    cols = ['r','b','c','g', 'orange']
-    plt.pie(slice,
-    labels =activities,
-    colors = cols,
-    startangle = 90,
-    shadow = True,
-    explode =(0,0.1,0,0,0),
-    autopct ='%1.1f%%')
-    plt.title('Training Subjects')
-
-    plt.savefig("output2.png", bbox_inches='tight')
-
-
-def area_plot():
-    plt.figure(figsize=(10,6))
-
-    days = [1,2,3,4,5]
-    age =[63, 81, 52, 22, 37]
-    weight =[17, 28, 72, 52, 32]
-    plt.plot([],[], color='c', label = 'Weather Predicted', linewidth=5)
-    plt.plot([],[],color = 'g', label='Weather Change happened', linewidth=5)
-    plt.stackplot(days, age, weight, colors = ['c', 'g'])
-    plt.xlabel('Fluctuation with time')
-    plt.ylabel('Days')
-    plt.title('Weather report using Area Plot')
-    plt.legend()
-
-    plt.savefig("output2.png", bbox_inches='tight')
-
-
-def hist_plot():
+# Function to create a histogram
+def create_histogram(data, bins, x_label, y_label, title, label):
     """
-    To create scatter plots 
+    Create and display a histogram.
 
     Args:
-        xs: list of lists for the x-axis
-        ys: list of lists for the y-axis
-        labels: list of labels for each corresponding (x, y) pairs
-        colors: list of colors for each corresponding (x, y) pairs
-        xlabel: label for the x-axis
-        ylabel: label for the y-axis
-        title: title for the graph
-        out_path: path to save the  graph
-
-    Returns:
-        None
+    data (list): Data points for the histogram.
+    bins (int): Number of bins for the histogram.
+    x_label (str): Label for the X-axis.
+    y_label (str): Label for the Y-axis.
+    title (str): Title of the plot.
+    label (str): Label for the histogram in the legend.
     """
-    plt.figure(figsize=(10,6))
+    fig, ax = plt.subplots()
+    ax.hist(data, bins=bins, edgecolor='black', alpha=0.7, label=label)
+    ax.set_xlabel(x_label)
+    ax.set_ylabel(y_label)
+    ax.set_title(title)
+    ax.legend()
 
-    pop = [22,55,62,45,21,22,34,42,42,4,2,8]
-    bins = [1,10,20,30,40,50]
-    plt.hist(pop, bins, rwidth=0.6)
-    plt.xlabel('age groups')
-    plt.ylabel('Number of people')
-    plt.title('Histogram')
-    
-    plt.savefig("output2.png", bbox_inches='tight')
+# Function to create a pie chart
+def create_pie_chart(sizes, labels, title):
+    """
+    Create and display a pie chart.
 
+    Args:
+    sizes (list): Sizes (values) for each slice of the pie.
+    labels (list): Labels for each slice.
+    title (str): Title of the pie chart.
+    """
+    fig, ax = plt.subplots()
+    ax.pie(sizes, labels=labels, autopct='%1.1f%%', startangle=90)
+    ax.axis('equal')  # Equal aspect ratio ensures that the pie is drawn as a circle.
+    ax.set_title(title)
 
+# Function to create a box plot
+def create_box_plot(data, labels, x_label, y_label, title):
+    """
+    Create and display a box plot.
 
+    Args:
+    data (list of lists): List of data for each box plot.
+    labels (list): Labels for each box plot.
+    x_label (str): Label for the X-axis.
+    y_label (str): Label for the Y-axis.
+    title (str): Title of the plot.
+    """
+    fig, ax = plt.subplots()
+    ax.boxplot(data, labels=labels)
+    ax.set_xlabel(x_label)
+    ax.set_ylabel(y_label)
+    ax.set_title(title)
 
+# Function to create an area plot (stacked line plot)
+def create_area_plot(x_values, y_values, x_label, y_label, title, labels):
+    """
+    Create and display an area plot (stacked line plot).
+
+    Args:
+    x_values (list): X-axis values.
+    y_values (list of lists): List of Y-axis values for each series.
+    x_label (str): Label for the X-axis.
+    y_label (str): Label for the Y-axis.
+    title (str): Title of the plot.
+    labels (list): Labels for each series in the legend.
+    """
+    fig, ax = plt.subplots()
+    ax.stackplot(x_values, y_values, labels=labels)
+    ax.set_xlabel(x_label)
+    ax.set_ylabel(y_label)
+    ax.set_title(title)
+    ax.legend(loc='upper left')
+
+# Function to save a plot as an image
+def save_plot(figure, filename):
+    """
+    Save a plot as an image file.
+
+    Args:
+    figure (matplotlib.figure.Figure): The Matplotlib figure to save.
+    filename (str): The filename (including extension) to save the plot as.
+    """
+    figure.savefig(filename)
+    plt.close(figure)
