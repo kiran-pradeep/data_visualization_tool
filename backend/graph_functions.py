0092d84d8c4fec34254b28042be9732eed2dee87
Kiran Pradeep <kira@Kirans-MacBook-Air.local>
2023-11-23 14:45:46 +0530

    added fastapi functionality

--- backend/graph_functions.py
+++ backend/graph_functions.py
@@ -1,146 +1,143 @@
+from fastapi import FastAPI, HTTPException, Query, Path
 import matplotlib.pyplot as plt
+import io
+from fastapi.responses import StreamingResponse
+import numpy as np
 
-# Function to create a line plot
-def create_line_plot(x_values, y_values, x_label, y_label, title, label):
-    """
-    Create and display a line plot.
+app = FastAPI()
 
-    Args:
-    x_values (list): X-axis values.
-    y_values (list): Y-axis values.
-    x_label (str): Label for the X-axis.
-    y_label (str): Label for the Y-axis.
-    title (str): Title of the plot.
-    label (str): Label for the line in the legend.
-    """
+def generate_line_plot(x, y, title):
     fig, ax = plt.subplots()
-    ax.plot(x_values, y_values, label=label)
-    ax.set_xlabel(x_label)
-    ax.set_ylabel(y_label)
+    ax.plot(x, y, label='Line Plot')
+    ax.set_xlabel('X-axis')
+    ax.set_ylabel('Y-axis')
     ax.set_title(title)
     ax.legend()
+    return fig
 
-# Function to create a bar plot
-def create_bar_plot(categories, values, x_label, y_label, title, label):
-    """
-    Create and display a bar plot.
-
-    Args:
-    categories (list): Category names for the bars.
-    values (list): Values corresponding to each category.
-    x_label (str): Label for the X-axis.
-    y_label (str): Label for the Y-axis.
-    title (str): Title of the plot.
-    label (str): Label for the bars in the legend.
-    """
+def generate_bar_chart(x, y, title):
     fig, ax = plt.subplots()
-    ax.bar(categories, values, label=label)
-    ax.set_xlabel(x_label)
-    ax.set_ylabel(y_label)
+    ax.bar(x, y, label='Bar Chart')
+    ax.set_xlabel('X-axis')
+    ax.set_ylabel('Y-axis')
     ax.set_title(title)
     ax.legend()
+    return fig
 
-# Function to create a scatter plot
-def create_scatter_plot(x_values, y_values, x_label, y_label, title, label):
-    """
-    Create and display a scatter plot.
-
-    Args:
-    x_values (list): X-axis values.
-    y_values (list): Y-axis values.
-    x_label (str): Label for the X-axis.
-    y_label (str): Label for the Y-axis.
-    title (str): Title of the plot.
-    label (str): Label for the points in the legend.
-    """
+def generate_histogram(data, title):
     fig, ax = plt.subplots()
-    ax.scatter(x_values, y_values, label=label)
-    ax.set_xlabel(x_label)
-    ax.set_ylabel(y_label)
+    ax.hist(data, bins='auto', color='#0504aa', alpha=0.7, rwidth=0.85)
+    ax.set_xlabel('Value')
+    ax.set_ylabel('Frequency')
+    ax.set_title(title)
+    return fig
+
+def generate_scatter_plot(x, y, title):
+    fig, ax = plt.subplots()
+    ax.scatter(x, y, label='Scatter Plot')
+    ax.set_xlabel('X-axis')
+    ax.set_ylabel('Y-axis')
     ax.set_title(title)
     ax.legend()
+    return fig
 
-# Function to create a histogram
-def create_histogram(data, bins, x_label, y_label, title, label):
-    """
-    Create and display a histogram.
-
-    Args:
-    data (list): Data points for the histogram.
-    bins (int): Number of bins for the histogram.
-    x_label (str): Label for the X-axis.
-    y_label (str): Label for the Y-axis.
-    title (str): Title of the plot.
-    label (str): Label for the histogram in the legend.
-    """
-    fig, ax = plt.subplots()
-    ax.hist(data, bins=bins, edgecolor='black', alpha=0.7, label=label)
-    ax.set_xlabel(x_label)
-    ax.set_ylabel(y_label)
-    ax.set_title(title)
-    ax.legend()
-
-# Function to create a pie chart
-def create_pie_chart(sizes, labels, title):
-    """
-    Create and display a pie chart.
-
-    Args:
-    sizes (list): Sizes (values) for each slice of the pie.
-    labels (list): Labels for each slice.
-    title (str): Title of the pie chart.
-    """
+def generate_pie_chart(labels, sizes, title):
     fig, ax = plt.subplots()
     ax.pie(sizes, labels=labels, autopct='%1.1f%%', startangle=90)
-    ax.axis('equal')  # Equal aspect ratio ensures that the pie is drawn as a circle.
+    ax.axis('equal')
     ax.set_title(title)
+    return fig
 
-# Function to create a box plot
-def create_box_plot(data, labels, x_label, y_label, title):
-    """
-    Create and display a box plot.
-
-    Args:
-    data (list of lists): List of data for each box plot.
-    labels (list): Labels for each box plot.
-    x_label (str): Label for the X-axis.
-    y_label (str): Label for the Y-axis.
-    title (str): Title of the plot.
-    """
+def generate_box_plot(data, title):
     fig, ax = plt.subplots()
-    ax.boxplot(data, labels=labels)
-    ax.set_xlabel(x_label)
-    ax.set_ylabel(y_label)
+    ax.boxplot(data)
+    ax.set_xlabel('Data')
+    ax.set_ylabel('Value')
     ax.set_title(title)
+    return fig
 
-# Function to create an area plot (stacked line plot)
-def create_area_plot(x_values, y_values, x_label, y_label, title, labels):
-    """
-    Create and display an area plot (stacked line plot).
-
-    Args:
-    x_values (list): X-axis values.
-    y_values (list of lists): List of Y-axis values for each series.
-    x_label (str): Label for the X-axis.
-    y_label (str): Label for the Y-axis.
-    title (str): Title of the plot.
-    labels (list): Labels for each series in the legend.
-    """
+def generate_heatmap(data, title):
     fig, ax = plt.subplots()
-    ax.stackplot(x_values, y_values, labels=labels)
-    ax.set_xlabel(x_label)
-    ax.set_ylabel(y_label)
+    cax = ax.imshow(data, cmap='viridis')
+    fig.colorbar(cax)
     ax.set_title(title)
-    ax.legend(loc='upper left')
+    return fig
 
-# Function to save a plot as an image
-def save_plot(figure, filename):
-    """
-    Save a plot as an image file.
+def save_plot_to_stream(fig):
+    image_stream = io.BytesIO()
+    fig.savefig(image_stream, format='png')
+    image_stream.seek(0)
+    plt.close(fig)
+    return image_stream
 
-    Args:
-    figure (matplotlib.figure.Figure): The Matplotlib figure to save.
-    filename (str): The filename (including extension) to save the plot as.
-    """
-    figure.savefig(filename)
-    plt.close(figure)
+# API endpoints for different plot types
+
+@app.get("/generate_line_plot/")
+async def generate_line_plot_api(
+    x: list = Query(..., title="X-axis data", description="X-axis values", example="[1, 2, 3, 4, 5]"),
+    y: list = Query(..., title="Y-axis data", description="Y-axis values", example="[10, 12, 5, 8, 15]"),
+    title: str = Query(..., title="Plot Title", description="Title of the Line Plot")
+):
+    fig = generate_line_plot(x, y, title)
+    image_stream = save_plot_to_stream(fig)
+    return StreamingResponse(content=image_stream, media_type="image/png")
+
+@app.get("/generate_bar_chart/")
+async def generate_bar_chart_api(
+    x: list = Query(..., title="X-axis data", description="X-axis values", example="[1, 2, 3, 4, 5]"),
+    y: list = Query(..., title="Y-axis data", description="Y-axis values", example="[10, 12, 5, 8, 15]"),
+    title: str = Query(..., title="Plot Title", description="Title of the Bar Chart")
+):
+    fig = generate_bar_chart(x, y, title)
+    image_stream = save_plot_to_stream(fig)
+    return StreamingResponse(content=image_stream, media_type="image/png")
+
+@app.get("/generate_histogram/")
+async def generate_histogram_api(
+    data: list = Query(..., title="Data", description="List of values for the histogram", example="[1, 2, 2, 3, 3, 3, 4, 4, 5]"),
+    title: str = Query(..., title="Plot Title", description="Title of the Histogram")
+):
+    fig = generate_histogram(data, title)
+    image_stream = save_plot_to_stream(fig)
+    return StreamingResponse(content=image_stream, media_type="image/png")
+
+@app.get("/generate_scatter_plot/")
+async def generate_scatter_plot_api(
+    x: list = Query(..., title="X-axis data", description="X-axis values", example="[1, 2, 3, 4, 5]"),
+    y: list = Query(..., title="Y-axis data", description="Y-axis values", example="[10, 12, 5, 8, 15]"),
+    title: str = Query(..., title="Plot Title", description="Title of the Scatter Plot")
+):
+    fig = generate_scatter_plot(x, y, title)
+    image_stream = save_plot_to_stream(fig)
+    return StreamingResponse(content=image_stream, media_type="image/png")
+
+@app.get("/generate_pie_chart/")
+async def generate_pie_chart_api(
+    labels: list = Query(..., title="Labels", description="Labels for the Pie Chart", example='["Category A", "Category B", "Category C"]'),
+    sizes: list = Query(..., title="Sizes", description="Sizes for the Pie Chart", example="[30, 40, 30]"),
+    title: str = Query(..., title="Plot Title", description="Title of the Pie Chart")
+):
+    fig = generate_pie_chart(labels, sizes, title)
+    image_stream = save_plot_to_stream(fig)
+    return StreamingResponse(content=image_stream, media_type="image/png")
+
+@app.get("/generate_box_plot/")
+async def generate_box_plot_api(
+    data: list = Query(..., title="Data", description="List of values for the Box Plot", example="[1, 2, 3, 4, 5]"),
+    title: str = Query(..., title="Plot Title", description="Title of the Box Plot")
+):
+    fig = generate_box_plot(data, title)
+    image_stream = save_plot_to_stream(fig)
+    return StreamingResponse(content=image_stream, media_type="image/png")
+
+@app.get("/generate_heatmap/")
+async def generate_heatmap_api(
+    data: list = Query(..., title="Data", description="2D array for the Heatmap", example="[[1, 2, 3], [4, 5, 6], [7, 8, 9]]"),
+    title: str = Query(..., title="Plot Title", description="Title of the Heatmap")
+):
+    # Convert the input data to a numpy array
+    data = np.array(data, dtype=float)
+    
+    fig = generate_heatmap(data, title)
+    image_stream = save_plot_to_stream(fig)
+    return StreamingResponse(content=image_stream, media_type="image/png")
